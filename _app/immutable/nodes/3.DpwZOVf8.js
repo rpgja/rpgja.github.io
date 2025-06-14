7d6620aeeb68317b09ad5fde44fa786a5950e4c4
onjmin <onjmin@users.noreply.github.com>
2025-06-14 13:44:40 +0000

    deploy: b0909fec3861c33646d50541e094b763b3ec3c20

--- _app/immutable/nodes/3.DpwZOVf8.js
+++
@@ -1,2 +0,0 @@
-import{f as e,a}from"../chunks/D8Pq6A11.js";import"../chunks/34bkBYqM.js";import{n as r}from"../chunks/BezkS9w-.js";var p=e(`<h1>404だよん</h1> <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation</p>`,1);function i(t){var o=p();r(2),a(t,o)}export{i as component};
