c7fc42aa7532769f5c4b2d35086e9e7334c007d5
onjmin <onjmin@example.com>
2025-06-15 00:02:58 +0900

    fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,16 +17,16 @@
 	<meta property="og:description" content={description} />
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="{base}/" />
-	<meta property="og:image" content="{base}/momoi.png" />
+	<meta property="og:image" content="{base}/list.png" />
 
 	<!-- Twitter Card -->
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:title" content={title} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content="{base}/momoi.png" />
+	<meta name="twitter:image" content="{base}/list.png" />
 
 	<!-- Apple Touch Icon -->
-	<link rel="apple-touch-icon" href="{base}/momoi.png" />
+	<link rel="apple-touch-icon" href="{base}/list.png" />
 
 	<!-- PWA 対応 -->
 	<link rel="manifest" href="{base}/manifest.json" />
