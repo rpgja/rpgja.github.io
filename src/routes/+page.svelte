cc25192b17d20164c07ccebd8c45737878514ef7
onjmin <onjmin@example.com>
2025-06-15 00:38:40 +0900

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { ExternalLinkIcon } from "@lucide/svelte";
+    import { base } from "$app/paths";
 
     // サイトデータ（今後増やせる）
     const sites = [
@@ -34,7 +35,14 @@
 </script>
 
 <section class="max-w-3xl mx-auto p-4 space-y-4">
-    <h2 class="text-2xl font-bold mb-6">RPGEN非公式サイト一覧</h2>
+    <h2 class="flex items-center text-2xl font-bold mb-6">
+        <img
+            src="{base}/list.png"
+            alt="Midori Icon"
+            class="w-16 h-16 object-contain mr-2"
+        />
+        RPGEN非公式サイト一覧
+    </h2>
     <p class="opacity-60">あなたのRPG制作を応援します。</p>
     <ul class="space-y-4">
         {#each sites as site}
